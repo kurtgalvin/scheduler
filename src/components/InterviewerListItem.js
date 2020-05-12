0bab31d3412950324d6ed0c374deeeeefab70f20
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-05-12 14:12:43 -0700

    Build out dynamic InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import classNames from 'classnames'
+
+import 'components/InterviewerListItem.scss'
 
 export default function(props) {
+  const className = classNames('interviewers__item', {'interviewers__item--selected': props.selected})
+
   return (
-    <li className="interviewers__item">
+    <li className={className} onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
+        src={props.avatar}
         alt="Sylvia Palmer"
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   )
 }
