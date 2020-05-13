bdd16e810cbc22dc452662bacada80fe8702e679
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-05-12 18:31:45 -0700

    Appointment init

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './styles.scss'
+
+const Appointment = function(props) {
+  return (
+    <article className="appointment"></article>
+  )
+}
+
+export default Appointment
