b69745815ea83c8655e85cf55d0b9a878bdbbdc7
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-05-14 17:54:59 -0700

    Create getInterviewersForDay and pass tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,6 +6,14 @@ export function getAppointmentsForDay(state, day) {
   return []
 }
 
+export function getInterviewersForDay(state, day) {
+  const dayObj = state.days.find(dayObj => dayObj.name === day)
+  if (dayObj) {
+    return dayObj.interviewers.map(id => state.interviewers[id])
+  }
+  return []
+}
+
 export function getInterview(state, interview) {
   if (interview) {
     const interviewer = state.interviewers[interview.interviewer]
