d240a431cb0efaa861727095e0d9f0052861d695
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-05-15 14:46:53 -0700

    Refactor state management out of Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,61 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import React from "react";
 
 import "components/Application.scss";
 import DayList from 'components/DayList'
 import Appointment from 'components/Appointment'
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from 'helpers/selectors'
+import useApplicationData from '../hooks/useApplicationData'
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday", 
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then(([days, appointments, interviewers]) => {
-      setState({
-        ...state, 
-        days: days.data, 
-        appointments: appointments.data,
-        interviewers: interviewers.data
-      })
-    })
-  }, [])
-
-  const setDay = day => setState({...state, day})
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`/api/appointments/${id}`, { interview })
-      .then(() => setState({...state, appointments}))
-  }
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => setState({...state, appointments}))
-  }
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   return (
     <main className="layout">
